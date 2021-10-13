7f0dc55795ea4d31252bc25810129aea7178b2e4
Michael Kutateladze <michael.kutateladze@yandex.ru>
2021-10-13 19:34:00 +0300

    Michaeladze (#20)
    * fix: [michaeladze] fix tooltip color
    
    * fix: [michaeladze] pageWithList for redesign
    
    * fix: [michaeladze] fix: [michaeladze] pageWithList for redesign

---
+++ src/components/organisms/ActionMenu/ActionMenu.tsx
@@ -0,0 +1,76 @@
+import React, { ReactNode } from 'react';
+import './ActionMenu.scss';
+import ChevronDown from '../../../assets/icons/ChevronDown';
+import { IOption } from '../../../types';
+import { IActionMenuListConfig } from '../../../types/projects.types';
+import {
+  Button, Input, Menu, Select
+} from '../../../index';
+
+interface IActionMenuProps {
+  type?: 'default' | 'list' | 'action';
+  listConfig?: IActionMenuListConfig;
+  children?: ReactNode | ReactNode[];
+}
+
+const ActionMenu: React.FC<IActionMenuProps> = ({
+  type = 'default',
+  listConfig,
+  children
+}: IActionMenuProps) => {
+
+
+  // -------------------------------------------------------------------------------------------------------------------
+
+  const getSortValue = (o: IOption[]) => {
+    listConfig?.onSort(o[0].value);
+  };
+
+  const handleSearch = (e: React.KeyboardEvent) => {
+    listConfig?.onSearch((e.target as HTMLInputElement).value);
+  };
+
+  const listJSX = listConfig && (
+    <div className='jf-action-menu__header'>
+      <div className='jf-action-menu__sorting'>
+        { listConfig.sortList.length > 0 && listConfig &&
+        <Select readOnly options={ listConfig.sortList }
+          values={ listConfig.defaultSortValue ? [listConfig.defaultSortValue] : [listConfig.sortList[0]] }
+          onChange={ getSortValue }/> }
+      </div>
+      <div className='jf-action-menu__search'>
+        <Input onKeyUp={ handleSearch } placeholder='Поиск' onClear={ listConfig.onClear }/>
+      </div>
+      {
+        listConfig.singleAction && (
+          <div className='jf-action-menu__list-button'>
+            <Button onClick={ listConfig.singleAction }>
+              { listConfig.actionLabel || 'Создать' }
+            </Button>
+          </div>
+        )
+      }
+      { !listConfig.singleAction && listConfig.actionList.length > 0 && (
+        <div className='jf-action-menu__list-button'>
+          <Menu list={ listConfig.actionList } position='right'>
+            <Button>
+              <div className='jf-action-menu__button-inner'>
+                { listConfig.actionLabel || 'Создать' } <ChevronDown className='jf-action-menu__list-button-icon'/>
+              </div>
+            </Button>
+          </Menu>
+        </div>
+      ) }
+    </div>
+  );
+
+  // -------------------------------------------------------------------------------------------------------------------
+
+  return (
+    <div className={ `jf-action-menu jf-action-menu--${type}` }>
+      { type === 'list' ? listJSX : children }
+    </div>
+  );
+};
+
+export default ActionMenu;
