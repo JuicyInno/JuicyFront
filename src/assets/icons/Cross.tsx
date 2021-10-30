be700574cc4b367e26f7a043ee7f85e7e89ebc54
unknown <mun4kin@gmail.com>
2021-10-30 12:15:02 +0300

    fix: [hotfix] fix linter and signification

--- src/assets/icons/Cross.tsx
+++ src/assets/icons/Cross.tsx
@@ -1,5 +1,5 @@
 import React, { SVGProps } from 'react';
-
+/* eslint-disable max-len */
 export default (props: SVGProps<SVGSVGElement>) =>
   <svg width='12' height='12' viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
     <path fillRule='evenodd' clipRule='evenodd' d='M0.412047 0.41107C0.737484 0.085633 1.26512 0.085633 1.59056 0.41107L11.5906 10.4111C11.916 10.7365 11.916 11.2641 11.5906 11.5896C11.2651 11.915 10.7375 11.915 10.412 11.5896L0.412047 1.58958C0.0866104 1.26414 0.0866104 0.736507 0.412047 0.41107Z' fill='currentColor'/>
--- src/components/atoms/ButtonGroup/ButtonGroup.stories.tsx
+++ src/components/atoms/ButtonGroup/ButtonGroup.stories.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { buttonGroupMock } from './mock';
 
 export default {
-  title: 'controls/Группа кнопок',
+  title: 'controls/ButtonGroup',
   component: ButtonGroup,
 };
 
