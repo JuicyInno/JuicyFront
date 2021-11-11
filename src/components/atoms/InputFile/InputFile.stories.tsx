557c0f1e5ef4b461a23ffa1134669adfde287928
dangurova <71314165+dangurova@users.noreply.github.com>
2021-11-11 13:03:33 +0300

    Fix/input file (#102)

--- src/components/atoms/InputFile/InputFile.stories.tsx
+++ src/components/atoms/InputFile/InputFile.stories.tsx
@@ -1,46 +1,80 @@
 import React from 'react';
+import { Story } from '@storybook/react';
+
 import { IFileData } from '../../../types';
-import Story from '../../storybook/Story';
+import { initialFiles } from '../../../utils/helpers';
 import StoryRow from '../../storybook/StoryRow';
 import StoryItem from '../../storybook/StoryItem';
-import InputFile from './InputFile';
+import InputFile, { IFileInputProps } from './InputFile';
+import { StoryDocsH1, StoryDocs } from '../../storybook';
+import StoryContainer from '../../storybook/Story';
+
 
 export default {
-  title: 'forms/не проверено/InputFile',
-  component: InputFile
+  title: 'forms/withTest/InputFile',
+  component: InputFile,
+  argTypes: {
+    files: { control: null },
+    placeholder: { defaultValue: 'Выберите файлы' },
+    name: { defaultValue: 'file' },
+    className: { control: null },
+    customPlaceholder: { control: null },
+    type: { control: null },
+    startAdornment: { control: null },
+    endAdornment: { control: null },
+    pressedCondition: { control: null },
+  }
 };
 
-export const inputFile = () => {
-  /** Получаем файл */
-  const setFile = (file: IFileData[]) => {
-    console.log(file);
-  };
+/** Получаем файл */
+const setFile = (file: IFileData[]) => {
+  console.log(file);
+};
+
+export const Demo: Story = () => {
 
   return (
-    <Story name='FileInput (Выбор файла)'>
+    <StoryDocs>
+      <StoryDocsH1>Input File</StoryDocsH1>
       <StoryItem description='Простой выбор файла'>
-        <InputFile name='file' placeholder='Выберите файлы' setFile={setFile} />
+        <InputFile name='file' buttonType='light' files={initialFiles} placeholder='Выберите файлы' setFile={setFile} />
       </StoryItem>
       <StoryItem description='Выбор определенного типа файлов'>
         <StoryRow>
-          <InputFile name='file' placeholder='Выберите pdf' accept='application/*' multiple setFile={setFile} />
+          <InputFile name='file' buttonType='light' placeholder='Выберите PDF' accept='application/pdf' multiple setFile={setFile} />
         </StoryRow>
         <StoryRow>
           <InputFile
             name='file'
             placeholder='Изображение PNG (50кБ)'
             accept='image/png'
+            buttonType='light'
             setFile={setFile}
-            max={50}
+            maxSize={50}
           />
-        </StoryRow>
-        <StoryRow>
-          <InputFile name='file' placeholder='Выберите текстовый файл' accept='text/*' setFile={setFile} />
         </StoryRow>
       </StoryItem>
       <StoryItem description='Неактивное поле'>
         <InputFile name='file' placeholder='Это поле неактивно' setFile={setFile} disabled={true} />
       </StoryItem>
-    </Story>
+    </StoryDocs>
   );
+};
+
+
+export const Playground: Story<IFileInputProps> = (args) => {
+  return (
+    <StoryContainer>
+      <StoryRow>
+        <InputFile {...args} setFile={setFile}/>
+      </StoryRow>
+    </StoryContainer>
+  );
+};
+
+Demo.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/Tl0AmqQJK4qaCl4pLRio7A/Design-System-for-Story-Book?node-id=18%3A17989',
+  },
 };
