c69d3d8d2aec172ac4644109c5136a30227ae926
Даньшина Анна Евгеньевна (4048118) <aedanshina@dev.vtb.ru>
2021-11-16 17:18:38 +0300

    refactor: [statusWithText] оформление стори

--- src/components/atoms/StatusWithText/StatusWithText.stories.tsx
+++ src/components/atoms/StatusWithText/StatusWithText.stories.tsx
@@ -3,8 +3,18 @@ import StatusWithText from './StatusWithText';
 import { StoryDocs, StoryDocsH1 } from '../../storybook';
 
 export default {
-  title: 'controls/не проверено/StatusWithText',
-  component: StatusWithText
+  title: 'controls/withTest/StatusWithText',
+  component: StatusWithText,
+  argTypes: {
+    statusText: {
+      control: 'text',
+      defaultValue: 'Заголовок'
+    },
+    criticality: {
+      type: 'string',
+      defaultValue: '0'
+    },
+  }
 };
 
 const statuses = [
@@ -26,7 +36,7 @@ const statuses = [
   }
 ];
 
-export const status = () => <StoryDocs>
+export const Demo = () => <StoryDocs>
   <StoryDocsH1>Status with text</StoryDocsH1>
   <div style={{ display: 'flex' }}>
     {statuses.map(item => <div style={{ padding: 4 }}>
@@ -36,3 +46,12 @@ export const status = () => <StoryDocs>
     </div>)}
   </div>
 </StoryDocs>;
+
+Demo.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/Tl0AmqQJK4qaCl4pLRio7A/Design-System-for-Story-Book?node-id=1223%3A24006',
+  },
+  actions: { disabled: true },
+  controls: { disabled: true }
+};
