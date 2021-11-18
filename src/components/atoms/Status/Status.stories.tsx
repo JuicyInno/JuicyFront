663064279081c1342831f56750d10e7fe94430ae
Даньшина Анна Евгеньевна (4048118) <aedanshina@dev.vtb.ru>
2021-11-18 13:34:54 +0300

    feat: [statusNew] добавила в сторю блок с демострацией размеров икнонок

--- src/components/atoms/Status/Status.stories.tsx
+++ src/components/atoms/Status/Status.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Status, { IconType } from './Status';
 import { StoryDocs, StoryDocsH1 } from '../../storybook';
+import { Size } from '../../../types';
 
 export default {
   title: 'controls/withTest/Status',
@@ -23,9 +24,65 @@ const icons: IconType[] = [
   'boss'
 ];
 
+const demoIcons = [
+  {
+    size: 'xxs',
+    sizeNumber: 16
+  },
+  {
+    size: 'xs',
+    sizeNumber: 20
+  },
+  {
+    size: 's',
+    sizeNumber: 24
+  }
+];
+
 export const Demo = () => (
   <StoryDocs>
     <StoryDocsH1>Status</StoryDocsH1>
+    <div style={{
+      display: 'flex',
+      width: 200,
+      marginBottom: 50,
+      backgroundColor: '#F1F2F4',
+      padding: 24,
+      borderRadius: 16
+    }}>
+      {demoIcons.map(icon => (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginRight: 28
+        }}>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}>
+            <div style={{
+              padding: '0px 4px',
+              color: 'white',
+              backgroundColor: '#2ABB5B',
+              borderRadius: 2,
+              marginBottom: 4
+            }}>
+              {icon.sizeNumber}
+            </div>
+            <div style={{
+              width: icon.sizeNumber + 4,
+              height: 12,
+              borderTop: '2px solid #2ABB5B',
+              borderLeft: '2px solid #2ABB5B',
+              borderRight: '2px solid #2ABB5B'
+            }} />
+          </div>
+          <Status type='disease' size={icon.size as Size}/>
+        </div>
+      ))}
+    </div>
     <div style={{ display: 'flex' }}>
       {icons.map(icon => <div style={{ marginRight: 8 }}><Status type={icon} size='xs'/></div>)}
     </div>
