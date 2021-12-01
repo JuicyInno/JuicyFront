1870ad97c6ae2b7e67797686f2ea2d9f428884af
Русак Александр Владимирович (4055089) <aliaksandr.rusak@mbicycle.com>
2021-12-01 12:48:09 +0300

    [rav] tooltip

--- src/components/atoms/Tooltip/Tooltip.tsx
+++ src/components/atoms/Tooltip/Tooltip.tsx
@@ -7,7 +7,7 @@ import { TooltipPosition } from '../../../types/projects.types';
 import { extractTextFromHTML } from '../../../utils/helpers';
 import { renderToStaticMarkup } from 'react-dom/server';
 
-interface ITooltipContentProps {
+export interface ITooltipContentProps {
   rect: DOMRect;
   children: ReactNode | ReactNode[];
   position: TooltipPosition;
@@ -97,11 +97,39 @@ const TooltipContent: FC<ITooltipContentProps> = ({ rect, children, position, cl
   };
   const SvgImageUnion = (color: 'default' | 'white' = 'default', position: TooltipPosition = 'bottom') => {
     return <div className={`rf-tooltip__content-union--${position}`}>
-      {color === 'white' ? <svg width='25' height='15' viewBox='0 0 25 15' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <path d='M24.5 0H0.5V8H1.09688C2.00516 8 2.8864 8.29089 3.59566 8.82483L11.2506 14.5876C11.981 15.1375 13.019 15.1375 13.7494 14.5876L21.4043 8.82483C22.1136 8.29089 22.9948 8 23.9031 8H24.5V0Z' fill='white' />
-      </svg> : <svg width='25' height='15' viewBox='0 0 25 15' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <path d='M24.5 0H0.5V8H1.09688C2.00516 8 2.8864 8.29089 3.59566 8.82483L11.2506 14.5876C11.981 15.1375 13.019 15.1375 13.7494 14.5876L21.4043 8.82483C22.1136 8.29089 22.9948 8 23.9031 8H24.5V0Z' fill='#8A96A8' />
-      </svg>
+      {color === 'white' ? <svg width='30' height='13' viewBox='0 0 30 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <g filter='url(#filter0_d_1829_24280)'>
+          <path d='M27 0H3C3.90829 0 5.3864 0.290891 6.09566 0.824826L13.7506 6.58759C14.481 7.13747 15.519 7.13747 16.2494 6.58759L23.9043 0.824826C24.6136 0.290891 26.0917 0 27 0Z' fill='white' />
+        </g>
+        <defs>
+          <filter id='filter0_d_1829_24280' x='0' y='0' width='30' height='13' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha' />
+            <feOffset dy='3' />
+            <feGaussianBlur stdDeviation='1.5' />
+            <feColorMatrix type='matrix' values='0 0 0 0 0.54 0 0 0 0 0.588387 0 0 0 0 0.66 0 0 0 0.28 0' />
+            <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_1829_24280' />
+            <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_1829_24280' result='shape' />
+          </filter>
+        </defs>
+      </svg> :
+        <svg width='30' height='13' viewBox='0 0 30 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
+          <g filter='url(#filter0_d_1829_26167)'>
+            <path d='M27 0H3C3.90829 0 5.3864 0.290891 6.09566 0.824826L13.7506 6.58759C14.481 7.13747 15.519 7.13747 16.2494 6.58759L23.9043 0.824826C24.6136 0.290891 26.0917 0 27 0Z' fill='#8A96A8' />
+          </g>
+          <defs>
+            <filter id='filter0_d_1829_26167' x='0' y='0' width='30' height='13' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+              <feFlood flood-opacity='0' result='BackgroundImageFix' />
+              <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha' />
+              <feOffset dy='3' />
+              <feGaussianBlur stdDeviation='1.5' />
+              <feColorMatrix type='matrix' values='0 0 0 0 0.54 0 0 0 0 0.588387 0 0 0 0 0.66 0 0 0 0.28 0' />
+              <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_1829_26167' />
+              <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_1829_26167' result='shape' />
+            </filter>
+          </defs>
+        </svg>
+
       }
     </div>;
   };
@@ -160,6 +188,7 @@ const Tooltip: FC<ITooltipProps> = ({
     setTooltipRect(null);
   }, []);
 
+
   const addListener = (add: boolean) => {
     if (add) {
       window.addEventListener('mousewheel', onScrollElementScroll);
@@ -208,6 +237,7 @@ const Tooltip: FC<ITooltipProps> = ({
           {children[1]}
         </TooltipContent>
       )}
+
     </div>
   );
 };
