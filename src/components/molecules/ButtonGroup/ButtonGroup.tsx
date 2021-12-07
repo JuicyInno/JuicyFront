4e6b7c7cdc630482cb598377d5f6687d1ae4b81c
Andrei Kudel <andrei.kudel@gmail.com>
2021-12-07 16:18:14 +0300

    fix: Tooltip (use react popper)

--- src/components/molecules/ButtonGroup/ButtonGroup.tsx
+++ src/components/molecules/ButtonGroup/ButtonGroup.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './ButtonGroup.scss';
 import { IButtonGroup } from '../../../types';
-import { Button, ChevronLeft, KebabMenu } from '../../../index';
+import {
+  Button, ChevronLeft, KebabMenu
+} from '../../../index';
 import Menu from '../../atoms/Menu';
 import { Link } from 'react-router-dom';
 import Tile from '../../atoms/Tile';
@@ -25,14 +27,12 @@ const ButtonGroup: React.FC<IButtonGroupProps> = ({ list, max = 2 }: IButtonGrou
 
   for (let i = 0; i < m; i++) {
     if (list[i].component) {
-      buttonsJSX.push(
-        <div className='button-group__item' key={i}>
-          <Tooltip portal position='bottom'>
-            {list[i].component}
-            {list[i].tooltip}
-          </Tooltip>
-        </div>
-      );
+      buttonsJSX.push(<div className='button-group__item' key={i}>
+        <Tooltip position='bottom'>
+          {list[i].component}
+          {list[i].tooltip}
+        </Tooltip>
+      </div>);
     }
   }
 
@@ -52,15 +52,13 @@ const ButtonGroup: React.FC<IButtonGroupProps> = ({ list, max = 2 }: IButtonGrou
         }
       };
 
-      menuJSX.push(
-        <Link to={list[i].url || '/'} className='button-group__menu-button' key={i} onClick={onClick}>
-          <div className='button-group__menu-button-details'>
-            <h4 className='button-group__menu-button-name'>{list[i].label}</h4>
-            {list[i].description && <p className='button-group__menu-button-description'>{list[i].description}</p>}
-          </div>
-          <ChevronLeft className='button-group__menu-button-icon' />
-        </Link>
-      );
+      menuJSX.push(<Link to={list[i].url || '/'} className='button-group__menu-button' key={i} onClick={onClick}>
+        <div className='button-group__menu-button-details'>
+          <h4 className='button-group__menu-button-name'>{list[i].label}</h4>
+          {list[i].description && <p className='button-group__menu-button-description'>{list[i].description}</p>}
+        </div>
+        <ChevronLeft className='button-group__menu-button-icon' />
+      </Link>);
     }
   }
 
@@ -75,7 +73,7 @@ const ButtonGroup: React.FC<IButtonGroupProps> = ({ list, max = 2 }: IButtonGrou
       {buttonsJSX}
       {menuJSX.length > 0 && (
         <Menu content={<div className='button-group__menu'>{menuJSX}</div>} position='top-start'>
-          <Tooltip portal position='bottom'>
+          <Tooltip position='bottom'>
             <Button buttonType='light' size='l' data-testid='button-group__more' startAdornment={<KebabMenu />}></Button>
             <>Другие действия</>
           </Tooltip>
