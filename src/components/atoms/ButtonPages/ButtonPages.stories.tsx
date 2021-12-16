79d0eaa87f753755796108b6084f8976030a3c6f
EdCast Dunice <edcastdunice@Mac-mini-EdCast-2.local>
2021-12-16 12:31:04 +0300

    fix: [buttonPages] add figma link to design tab

--- src/components/atoms/ButtonPages/ButtonPages.stories.tsx
+++ src/components/atoms/ButtonPages/ButtonPages.stories.tsx
@@ -42,6 +42,6 @@ export const Playground: Story = (args) => {
 Demo.parameters = {
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/Y86V3oIhkZQ4u27iBuhN0l/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D1%8B-%D0%92%D0%A2%D0%91-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D0%BE%D0%B9?node-id=13613%3A74776'
+    url: 'https://www.figma.com/file/Tl0AmqQJK4qaCl4pLRio7A/Design-System-for-Story-Book?node-id=18%3A23793'
   }
 };
