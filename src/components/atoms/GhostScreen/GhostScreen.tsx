bcdeec47d31eef5f556ebaf63574b83385ee79db
Даньшина Анна Евгеньевна (4048118) <aedanshina@dev.vtb.ru>
2022-02-07 14:42:21 +0300

    refactor: [iconsMoving] убрала лишнюю иллюстрацию

--- src/components/atoms/GhostScreen/GhostScreen.tsx
+++ src/components/atoms/GhostScreen/GhostScreen.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './GhostScreen.scss';
-import GhostImage from '../../../assets/illustrations/Ghost';
+import GhostImage from '../../../assets/illustrations/ghost.png';
 
 export interface IGhostProps {
   text?: string;
-
 }
 
 const GhostScreen: React.FC<IGhostProps> = ({ text = 'Задачи не найдены' }: IGhostProps) => {
 
   return (
     <div className='ghost__wrapper'>
-      <GhostImage/>
+      <img src={GhostImage} width={220} height={170} />
       <div className='ghost__text'>
         {text}
       </div>
