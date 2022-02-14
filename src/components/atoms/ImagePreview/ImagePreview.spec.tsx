f13fcf1c88dfbc513870335526716b2f7c9746c6
Русак Александр Владимирович (4055089) <avrusak@dev.vtb.ru>
2022-02-14 17:08:58 +0300

    [rav] feat/ image preview

--- src/components/atoms/ImagePreview/ImagePreview.spec.tsx
+++ src/components/atoms/ImagePreview/ImagePreview.spec.tsx
@@ -3,4 +3,8 @@ import { render } from '@testing-library/react';
 import ImagePreview from './ImagePreview';
 
 describe('Test <ImagePreview/> component', () => {
+  it('should work as uncontrolled component', () => {
+    const { container } = render(<ImagePreview imageList={['https://uprostim.com/wp-content/uploads/2021/01/image165-15.jpg']} onClose={() => { }} />);
+    expect(document.getElementsByClassName('rf-image-preview')).toHaveLength(1)
+  });
 });
