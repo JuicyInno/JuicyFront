ba352de1a551fd2f0ccdd0ea6dabf0c0efbf06ba
KudelAndrei <kudelko@dev.vtb.ru>
2022-02-23 13:27:00 +0300

    fix IControlProps

--- src/components/controls/types.ts
+++ src/components/controls/types.ts
@@ -1,8 +1,10 @@
-import { UseControllerProps } from 'react-hook-form';
+import { Control, UseControllerProps } from 'react-hook-form';
 import { IFormGroup } from '../atoms/FormGroup/FormGroup';
 import { IFormErrorControlProps } from './FormControl/FormControl';
 
-export type IControlProps = UseControllerProps & Pick<IFormErrorControlProps, 'formState'>;
+export type IControlProps = Omit<UseControllerProps, 'control'> & Pick<IFormErrorControlProps, 'formState'> & {
+  control?: Control<Record<string, any>>;
+};
 
 export type IPartialFormGroup =
   Omit<IFormGroup, 'children' | 'label' | 'errorMessage' | 'invalid' | 'required'>;
